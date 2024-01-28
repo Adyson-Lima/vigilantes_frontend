36829a5bb296c8c0563c961f7f3ce9c18988ec13
Adyson-Lima <adyson_lima@yahoo.com.br>
2024-01-28 10:00:36 -0300

    27° commit - logica saveOrUpdate implementada

--- src/pages/NewUpdate/index.js
+++ src/pages/NewUpdate/index.js
@@ -1,8 +1,60 @@
 import { Link, useNavigate, useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
-// import api from '../../services/api';
+import api from '../../services/api';
 
 export default function NewUpdate(){
+
+  const [name, setName] = useState('');
+  const [vehicle, setVehicle] = useState('');
+  const navigate = useNavigate();
+
+  // vigilante_id definido na rota NewUpdate
+  const {vigilante_id} = useParams();
+
+  // recebe e manipula eventos do form
+  async function saveOrUpdate(e){
+
+    e.preventDefault();
+
+    const data = {name, vehicle};
+
+    if (vigilante_id === '0') {
+      try {
+        await api.post('api/v1/vigilantes', data, {});
+        navigate('/');
+      } catch (error) {
+        alert('erro ao salvar');        
+      }      
+    } else {
+      try {
+        await api.patch(`api/v1/vigilantes/${vigilante_id}`, data, {});
+        navigate('/');
+      } catch (error) {
+        alert('erro ao salvar');        
+      }      
+    }
+  }
+
+  // carrega registro especifico na api e seta dados para atualização
+  async function loadVigilante(){
+    try {
+      const response = await api.get(`api/v1/vigilantes/${vigilante_id}`,{});
+      setName(response.data.name);
+      setVehicle(response.data.vehicle);
+    } catch (error) {
+      alert('Erro ao atualizar');
+      navigate('/');      
+    }
+  }
+
+  // chama loadVigilante e preenche form
+  useEffect(() => {
+    if (vigilante_id === '0') {
+      return;      
+    } else {
+      loadVigilante();      
+    }
+  }, [vigilante_id]);
 
   return(
 
@@ -15,20 +67,22 @@ export default function NewUpdate(){
         <Link data-testid="mylink" className="btn btn-dark" 
         style={{marginBottom: '5px'}} to="/">Home</Link>
 
-        <form data-testid="myform">
+        <form data-testid="myform" onSubmit={saveOrUpdate}>
           
           <div className="form-group">
-            <label htmlFor="###">###</label>
-            <input data-testid="input1" id="###" type="text" 
+            <label htmlFor="name">Nome</label>
+            <input data-testid="input1" id="name" type="text" 
             style={{marginBottom: '20px'}} className="form-control" 
-            placeholder="###"></input>
+            placeholder="Nome" value={name}
+            onChange={e => setName(e.target.value)}></input>
           </div>
 
           <div className="form-group">
-            <label htmlFor="###">###</label>
-            <input data-testid="input2" id="###" type="text" 
+            <label htmlFor="vehicle">Veiculo</label>
+            <input data-testid="input2" id="vehicle" type="text" 
             style={{marginBottom: '20px'}} className="form-control" 
-            placeholder="###"></input>
+            placeholder="Veiculo" value={vehicle}
+            onChange={e => setVehicle(e.target.value)}></input>
           </div>
 
           <button data-testid="btnenviar" type="submit" className="btn btn-primary">Enviar</button>
