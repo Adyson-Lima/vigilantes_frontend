0b4bb6e8a2dc3846218d84dfaa15dc5614df24ae
Adyson-Lima <adyson_lima@yahoo.com.br>
2024-01-28 09:45:31 -0300

    26° commit - logica delete implementada

--- src/pages/Vigilantes/index.js
+++ src/pages/Vigilantes/index.js
@@ -22,6 +22,16 @@ export default function Vigilantes(){
     }
   }
 
+  // delete, exclui um registro na api
+  async function deleteVigilante(id){
+    try {
+      await api.delete(`api/v1/vigilantes/${id}`,{});
+      setVigilantes(my_vigilantes.filter(vigilante => vigilante.id !== id));
+    } catch (error) {
+      alert('erro ao excluir');      
+    }
+  }
+
   return(
 
     <div data-testid="mycard" className="card border-primary" style={{marginTop: '20px'}} >
@@ -55,7 +65,8 @@ export default function Vigilantes(){
                   onClick={() => updateVigilante(vigilante.id)}>Editar</button>
 
                   <button data-testid="mybtn2" type="button"
-                  className="btn btn-outline-danger" style={{margin: '2px'}}>Excluir</button>
+                  className="btn btn-outline-danger" style={{margin: '2px'}}
+                  onClick={() => deleteVigilante(vigilante.id)}>Excluir</button>
 
                 </td>
               </tr>
